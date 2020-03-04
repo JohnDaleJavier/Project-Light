be0874907879b44d1f0fe6c43505dd2bc24b8742
John Dale Javier <johndalejavier@gmail.com>
2020-03-04 17:10:55 -0600

    Title Screen WIP
    Working on Title Screen

---
+++ Project-Light/Assets/Scripts/Managers/TitleScreenManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public Scene[] scene;
+
+    public void StartGame()
+    {
+
+    }
+    public void Options()
+    {
+
+    }
+    public void Settings()
+    {
+
+    }
+    public void QuitGame()
+    {
+
+    }
+
+}
