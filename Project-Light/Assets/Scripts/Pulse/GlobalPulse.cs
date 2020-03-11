a3084f6a5d1b6474ff094d0867b6be5e17d740e0
JeromeTieh <61806805+JeromeTieh@users.noreply.github.com>
2020-03-11 11:03:40 +0900

    Added in the Pulse Mechanics

---
+++ Project-Light/Assets/Scripts/Pulse/GlobalPulse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalPulse : MonoBehaviour
+{
+    public GameObject lightningPulse;
+    public float pulseDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartCoroutine(lightning());
+        }
+    }
+
+
+    IEnumerator lightning()
+    {
+        lightningPulse.SetActive(true);
+
+        yield return new WaitForSeconds(pulseDuration);
+
+        lightningPulse.SetActive(false);
+    }
+}
---
+++ Project-Light/Assets/Scripts/Pulse/PlayerPulse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerPulse : MonoBehaviour
+{
+    public float pulseDuration;
+    public GameObject pulseLight;
+    PlayerMovement playerMovement;
+    int playerID;
+    Player player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent <PlayerMovement>();
+        playerID = playerMovement.playerId;
+        
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartCoroutine(pulse());
+        }
+    }
+
+    IEnumerator pulse()
+    {
+        pulseLight.SetActive(true);
+        yield return new WaitForSeconds(pulseDuration);
+        pulseLight.SetActive(false);
+    }
+}
